f5d59a536e7b6b027e4319a93d087ca5df5c2b21
Imran <imran@peanutlabs.com>
2013-10-02 20:28:14 -0700

    First commit

---
+++ wd_hw1/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WdHw1::Application.config.secret_key_base = '4d5ddaf7dd58f0567513a2587cac3a127e64b40b1abbcd2609a57ab8c30881ff01712c1aa7edbe5a4fdc95e16536f665efd4d3729a88f36cdc57c811a160e823'
